c7aa7870ffdbda0d12a4381d774e0b660a64d019
Luis Hernandez Gayo <entradaproyectos@gmail.com>
2020-09-08 19:53:58 +0200

    Fin Sección 6

---
+++ src/app/components/dona/dona.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input } from '@angular/core';
+import { MultiDataSet, Label, Color } from 'ng2-charts';
+
+
+@Component({
+  selector: 'app-dona',
+  templateUrl: './dona.component.html',
+  styles: [
+  ]
+})
+export class DonaComponent {
+
+  @Input() title:string = 'Sin título';
+
+      // Doughnut
+
+      @Input('labels') doughnutChartLabels: Label[] = ['label1', 'label2', 'label3'];
+
+      @Input('data') doughnutChartData: MultiDataSet = [
+    
+        [350, 450, 100]
+    
+      ];
+    
+      public colors: Color[] = [
+        { backgroundColor: [ '#9E120E', '#FF5800' , '#FFB414']}
+      ];
+
+  
+
+ 
+
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
+
+// modulos
+import { SharedModule } from '../shared/shared.module';
+import { ComponentsModule } from '../components/components.module';
 
 import { PagesComponent } from './pages.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { ProgressComponent } from './progress/progress.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { SharedModule } from '../shared/shared.module';
+
 
 
 
@@ -28,8 +32,10 @@ import { SharedModule } from '../shared/shared.module';
   ],
   imports: [
     CommonModule,
+    FormsModule,
     SharedModule,
-    RouterModule
+    RouterModule,
+    ComponentsModule
   ]
 })
 export class PagesModule { }
